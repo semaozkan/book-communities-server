8504a0f8ea40e9e152183d7a543c8cad3876144b
Semanur Özkan <semanurozkan4444@gmail.com>
2025-05-20 02:43:19 +0300

    feat: create post model

---
+++ models/PostModel.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    community: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        text: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const PostModel = mongoose.model("Post", postSchema);
+
+export default PostModel;
