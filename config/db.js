9a1607a09949fe03de6a6bac5ab83a006be5adfc
Semanur Özkan <semanurozkan4444@gmail.com>
2025-06-07 23:26:55 +0300

    refactor(db): simplify MongoDB connection logic and switch to ES module export

--- config/db.js
+++ config/db.js
@@ -1,16 +1,32 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   console.log(process.env.MONGO_URI);
+
+//   try {
+//     mongoose
+//       .connect(process.env.MONGO_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       })
+//       .then(() => console.log("MongoDB connected"))
+//       .catch((error) => console.error("Error connecting to MongoDB:", error));
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
